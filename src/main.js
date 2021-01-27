1c90523ad9e9d14b39577ee2e8a8941ddf258852
moran-aga <77380388+moran-aga@users.noreply.github.com>
2021-01-27 22:28:08 +0200

    add to-do function

--- src/main.js
+++ src/main.js
@@ -1,6 +1,30 @@
 const addButton = document.querySelector("#add-button");
-const list = document.querySelector("#list");
+const sortButton = document.querySelector("#sort-button");
+const list = document.querySelector(".todo-list");
 const input = document.querySelector("#text-input");
 
+addButton.addEventListener("click", addToDo)
+// todoList.addEventListener("click", deleteCheck)
+// sortButton.addEventListener("click", sortToDo)
 
-
+function addToDo(toDo) {
+  const text = `<li class="item">
+                    <i class="far fa-circle" job="complete"></i>
+                    <div class="todo-priority">
+                    1
+                    </div>
+                    <div class="todo-created-at">
+                    2020-06-18 11:51:12
+                    </div>
+                    <div class="todo-text">
+                    ${toDo}
+                    </div>
+                    <i class="far fa-trash-alt" job="delete"></i>
+                </li>`
+    
+    const position = "beforeend";
+    
+    list.insertAdjacentHTML(position, text);
+   
+}
+addToDo("eat");
