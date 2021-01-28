3bed03ccee444fd2413c04fc1ee5d418a6e34074
moran-aga <77380388+moran-aga@users.noreply.github.com>
2021-01-28 14:54:33 +0200

    remove item function

--- src/main.js
+++ src/main.js
@@ -5,16 +5,12 @@ const input = document.querySelector("#text-input");
 const priority = document.querySelector("#priority-selector");
 const title = document.querySelector("#title");
 const today = new Date();
-
 const CHECK = "far fa-check-circle";
 const UNCHECK = "far fa-circle";
-const LINETHROUGH ="lineThrough";
-
+// const LINETHROUGH ="lineThrough";
 let LIST= [];
 let id = 0;
 
-
- 
 function updateCount() {
     const count = list.childElementCount;
     title.innerHTML = `You have ${count} to-do's.`;
@@ -24,7 +20,7 @@ function addToDo(toDo, id, done, trash) {
 
     if(trash) { return; }
     const DONE = done ? CHECK : UNCHECK;
-    const LINE = done ? LINETHROUGH : "";
+    // const LINE = done ? LINETHROUGH : "";
 
     const item = `<li class="item">
     <i class="${DONE}" job="complete" id="${id}"></i>
@@ -34,7 +30,7 @@ function addToDo(toDo, id, done, trash) {
     <div class="todo-created-at">
     ${today}
     </div>
-    <div class="todo-text ${LINE}">
+    <div class="todo-text">
     ${toDo}
     </div>
     <i class="far fa-trash-alt" job="delete" id="${id}"></i>
@@ -61,7 +57,28 @@ addButton.addEventListener("click", function(event){
     updateCount();
 })
 
+function completeToDo(element){
+    element.classList.toggle(CHECK);
+    element.classList.toggle(UNCHECK);
+
+    LIST[element.id].done =  LIST[element.id].done ? false : true;
+}
+
+function removeToDo (element){
+    element.parentNode.parentNode.removeChild(element.parentNode);
+
+    LIST[element.id].trash = true;
+}
 
 
-// todoList.addEventListener("click", deleteCheck)
+list.addEventListener("click", function(event) {
+    const element = event.target;
+    const elementJob = element.attributes.job.value;
+
+    if (elementJob === "complete") {
+        completeToDo(element);
+    } else if (elementJob === "delete")
+        removeToDo(element);
+
+})
 // sortButton.addEventListener("click", sortToDo)
