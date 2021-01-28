d461a93184923f8a5ffa3c51156f8317bf37b45f
moran-aga <77380388+moran-aga@users.noreply.github.com>
2021-01-28 12:15:41 +0200

    counter function
    increas with each added to to item

--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ const addButton = document.querySelector("#add-button");
 const sortButton = document.querySelector("#sort-button");
 const list = document.querySelector(".todo-list");
 const input = document.querySelector("#text-input");
-const priority = document.querySelector("#priority-selector")
+const priority = document.querySelector("#priority-selector");
+const title = document.querySelector("#title");
+
+function updateCount() {
+    const count = list.childElementCount;
+    title.innerHTML = `You have ${count} to-dos.`;
+}
 
 addButton.addEventListener("click", function(event){
     const toDo = input.value;
@@ -10,28 +16,31 @@ addButton.addEventListener("click", function(event){
         addToDo(toDo);
     } 
     input.value = "";
-});
-// todoList.addEventListener("click", deleteCheck)
-// sortButton.addEventListener("click", sortToDo)
+    updateCount();
+})
 
 function addToDo(toDo) {
-  const text = `<li class="item">
-                    <i class="far fa-circle" job="complete"></i>
-                    <div class="todo-priority">
-                    ${priority.value}
-                    </div>
-                    <div class="todo-created-at">
-                    2020-06-18 11:51:12
-                    </div>
-                    <div class="todo-text">
-                    ${toDo}
-                    </div>
-                    <i class="far fa-trash-alt" job="delete"></i>
-                </li>`
+    const text = `<li class="item">
+    <i class="far fa-circle" job="complete"></i>
+    <div class="todo-priority">
+    ${priority.value}
+    </div>
+    <div class="todo-created-at">
+    2020-06-18 11:51:12
+    </div>
+    <div class="todo-text">
+    ${toDo}
+    </div>
+    <i class="far fa-trash-alt" job="delete"></i>
+    </li>`
     
     const position = "beforeend";
     
     list.insertAdjacentHTML(position, text);
-   
+    
 }
-addToDo("bla");
+
+let LIST=[];
+
+// todoList.addEventListener("click", deleteCheck)
+// sortButton.addEventListener("click", sortToDo)
