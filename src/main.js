f519460e31f1e8671104ebdaa9c434e442f0a52d
moran-aga <77380388+moran-aga@users.noreply.github.com>
2021-01-28 14:15:59 +0200

    array to store the toDo items

--- src/main.js
+++ src/main.js
@@ -6,25 +6,35 @@ const priority = document.querySelector("#priority-selector");
 const title = document.querySelector("#title");
 const today = new Date();
 
-const CHECK = "fa-check-circle";
-const UNCHECK = "fa-circle";
+const CHECK = "far fa-check-circle";
+const UNCHECK = "far fa-circle";
 const LINETHROUGH ="lineThrough";
+
+let LIST= [];
+let id = 0;
+
+
  
 function updateCount() {
     const count = list.childElementCount;
     title.innerHTML = `You have ${count} to-do's.`;
 }
 
-function addToDo(toDo, id) {
+function addToDo(toDo, id, done, trash) {
+
+    if(trash) { return; }
+    const DONE = done ? CHECK : UNCHECK;
+    const LINE = done ? LINETHROUGH : "";
+
     const item = `<li class="item">
-    <i class="far fa-circle" job="complete" id="${id}"></i>
+    <i class="${DONE}" job="complete" id="${id}"></i>
     <div class="todo-priority">
     ${priority.value}
     </div>
     <div class="todo-created-at">
     ${today}
     </div>
-    <div class="todo-text">
+    <div class="todo-text ${LINE}">
     ${toDo}
     </div>
     <i class="far fa-trash-alt" job="delete" id="${id}"></i>
@@ -32,20 +42,24 @@ function addToDo(toDo, id) {
     
     const position = "beforeend";
     
-    list.insertAdjacentHTML(position, item);
-    
+    list.insertAdjacentHTML(position, item);  
 }
+
 addButton.addEventListener("click", function(event){
     const toDo = input.value;
     if (toDo) {
-        addToDo(toDo);
+        addToDo(toDo, id, false, false);
+        LIST.push({
+            name: toDo,
+            id: id,
+            done: false,
+            trash: false
+        });
+        id++;
     } 
     input.value = "";
     updateCount();
 })
-
-let LIST=[];
-let id = 0;
 
 
 
